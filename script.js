0174ba8fad4bae8f2e62e818b953563706018e59
Mauricio Andrades <mod.and.op@gmail.com>
2015-11-04 11:48:47 -0800

    "finished the board"

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 // Your JS goes here
 
 //newDiv RED
-var newDiv = document.createElement("div");
-document.body.appendChild(newDiv);
-newDiv.className = "container";
-newDiv.style.paddingBottom = "11.1%";
-newDiv.style.backgroundColor = "red";
-newDiv.style.height = "50px";
-newDiv.style.width = "50px";
-newDiv.style.float = "left";
 
+var makeDiv = function(color) {
+    var newDiv = document.createElement("div");
+    document.body.appendChild(newDiv);
+    newDiv.className = "container";
+    newDiv.style.paddingBottom = "11.1%";
+    newDiv.style.backgroundColor = color;
+    newDiv.style.width = "11.1%";
+    newDiv.style.float = "left";
+    return newDiv;
+};
 
-
-
-
-
+for (i = 0 ; i< 9*12; i++) {
+    makeDiv("black");
+    makeDiv("red");
+}
 
 var body = document.getElementsByTagName("body");
 
