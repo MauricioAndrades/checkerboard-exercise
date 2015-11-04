cfdf72881a9d53e6f44ec5f4bd7d0f9d8912f4ae
Mauricio Andrades <mod.and.op@gmail.com>
2015-11-04 11:17:54 -0800

    got first div made

---
+++ min/script-min.js
@@ -0,0 +1,1 @@
+var newDiv=document.createElement("div");document.body.appendChild(newDiv),newDiv.className="container",newDiv.style.paddingBottom="11.1%",newDiv.style.backgroundColor="red",newDiv.style.height="50px",newDiv.style.width="50px",newDiv.style["float"]="left";var body=document.getElementsByTagName("body");
--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 // Your JS goes here
+
+//newDiv RED
+var newDiv = document.createElement("div");
+document.body.appendChild(newDiv);
+newDiv.className = "container";
+newDiv.style.paddingBottom = "11.1%";
+newDiv.style.backgroundColor = "red";
+newDiv.style.height = "50px";
+newDiv.style.width = "50px";
+newDiv.style.float = "left";
+
+
+
+
+
+
+
+var body = document.getElementsByTagName("body");
+
+
